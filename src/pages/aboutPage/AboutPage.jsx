c525b43ed10d670e9ea07ba4e0551ed338636eee
Kristy Thompson <105332803+Kristy-H-Thompson@users.noreply.github.com>
2023-11-26 11:47:58 -0700

    Update AboutPage.jsx

--- src/pages/aboutPage/AboutPage.jsx
+++ src/pages/aboutPage/AboutPage.jsx
@@ -1,1 +1,17 @@
+import React from 'react-dom/client';
+import Feature from '../../components/feature/Feature';
+import './aboutPage.css';
+import starstar from '../../assets/starstarSmile.png';
 
+
+const About = () => (
+  <section className="about">
+    <div className="heading">
+      <h1 className="gradient__text"> What can Starstar Do?</h1>
+      <p>This is where this paragraph will go</p>
+      <img className="starstarSmile" src={starstar} alt="Starstar"/>
+    </div>
+  </section>
+);
+
+export default About;
