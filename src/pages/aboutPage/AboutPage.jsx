0c778867e3e7ec2b9748eefa18cf02906111a03b
Kristy Thompson <105332803+Kristy-H-Thompson@users.noreply.github.com>
2023-11-26 12:27:45 -0700

    Update AboutPage.jsx

--- src/pages/aboutPage/AboutPage.jsx
+++ src/pages/aboutPage/AboutPage.jsx
@@ -11,8 +11,10 @@ const AboutPage = () => (
 
       <b>History</b>  
       <p>StarStar was orginally created for a Warriors roleplay site called <b>Warrior Cat Clans 2 </b> as an April Fool's joke in February 2020. Starstar <i>took over</i> the site's discord server for the day, fighting the website's bot Gene for control. Starstar became much beloved by the website's members, and lore began to build around the mysterious Starstar. When WCC2 closed down, Starstar was brought over and optimized for another roleplay site called <b>Sinister Haven</b></p>
-      <img className="starstarTongue" src={starstar} alt="Starstar"/>
     </div>
+
+          <img className="starstarTongue" src={starstar} alt="Starstar"/>
+
   </section>
 );
 
