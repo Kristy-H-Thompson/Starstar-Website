84e93baf84ad7eaf56916c55e189d9744b40af76
Kristy Thompson <105332803+Kristy-H-Thompson@users.noreply.github.com>
2023-11-26 12:29:33 -0700

    Update AboutPage.jsx

--- src/pages/aboutPage/AboutPage.jsx
+++ src/pages/aboutPage/AboutPage.jsx
@@ -6,14 +6,17 @@ import starstar from '../../assets/starstar-tongue.png';
 const AboutPage = () => (
   <section className="aboutPage">
     <div className="heading">
+      <div>
       <h1 className="gradient__text">Starstar</h1>
       <p>Starstar is a Discord Bot created and managed by Racer and Cleaver.</p>
 
       <b>History</b>  
       <p>StarStar was orginally created for a Warriors roleplay site called <b>Warrior Cat Clans 2 </b> as an April Fool's joke in February 2020. Starstar <i>took over</i> the site's discord server for the day, fighting the website's bot Gene for control. Starstar became much beloved by the website's members, and lore began to build around the mysterious Starstar. When WCC2 closed down, Starstar was brought over and optimized for another roleplay site called <b>Sinister Haven</b></p>
+      </div>
+      <img className="starstarTongue" src={starstar} alt="Starstar"/>
+
     </div>
 
-          <img className="starstarTongue" src={starstar} alt="Starstar"/>
 
   </section>
 );
