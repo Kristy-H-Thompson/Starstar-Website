c98b4136358ca1ee2a3a03bd1c04a7a611351186
Kristy Thompson <105332803+Kristy-H-Thompson@users.noreply.github.com>
2023-11-26 12:07:14 -0700

    Update AboutPage.jsx

--- src/pages/aboutPage/AboutPage.jsx
+++ src/pages/aboutPage/AboutPage.jsx
@@ -3,14 +3,14 @@ import './aboutPage.css';
 import starstar from '../../assets/starstar-tongue.png';
 
 
-const About = () => (
-  <section className="about">
+const AboutPage = () => (
+  <section className="aboutPage">
     <div className="heading">
       <h1 className="gradient__text"> What can Starstar Do?</h1>
       <p>This is where this paragraph will go</p>
-      <img className="starstarSmile" src={starstar} alt="Starstar"/>
+      <img className="starstarTongue" src={starstar} alt="Starstar"/>
     </div>
   </section>
 );
 
-export default About;
+export default AboutPage;
